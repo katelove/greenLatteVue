ef9321d336aa213787179c9bd64519a2d871be1c
Kate <katelovejesus1110@gmail.com>
2021-05-20 21:59:24 +0800

    相關設定

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 new Vue({
   router,
   store,
