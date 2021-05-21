1a4dbe9a193acb810f207e46394b0c9435e6cbc2
Kate <katelovejesus1110@gmail.com>
2021-05-21 22:26:22 +0800

    輪播圖

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div class="carousel">
+<section><Carousel/></section>
+</div>
+
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Carousel from '../components/Carousel'
 
 export default {
-  name: 'Home',
+  name: 'carousel',
   components: {
-    HelloWorld
+    Carousel
+
   }
 }
 </script>
+
+<style lang="scss">
+   * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+      }
+      html,
+      body {
+        height: 100%;
+        font-family: sans-serif;
+        font-weight: bold;
+      }
+         section {
+        min-height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+</style>
