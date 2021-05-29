fafda60f188a98152506757b6c2b7c3a3dc15e2d
Kate <katelovejesus1110@gmail.com>
2021-05-30 00:07:30 +0800

    畫板樣式調整

--- src/components/WeekProject.vue
+++ src/components/WeekProject.vue
@@ -1,41 +1,40 @@
 <template>
- <!-- <div class="container">
+ <div class="container">
    <div class="row">
-      <div class="col-sm-12 -col-md-12 col-lg-12 board-style"> -->
-      <div class="board-style">
+    <div class="col-sm-12 -col-md-12 col-lg-12 board-style">
       <!-- 工具列 -->
       <div id="tool">
-        <!-- <ul>
-        <li> -->
-          <div v-on:click="show = !show">
-              <h3>字級</h3>
-              <h1>A</h1>
-          </div>
-          <transition name="ul">
-            <ul v-if="show" class="ul-box">
-              <li><h2>A</h2></li>
-              <li><h4>A</h4></li>
-              <li><h6>A</h6></li>
-            </ul>
-          </transition>
-        <!-- </li> -->
-        <!-- <li>-->
-          <div v-on:click="active = ! active">
-              <h3>字的顏色</h3>
-              <div class="tool-color"></div>
-          </div>
-          <transition name="ul">
-            <ul v-if="active" class="ul-box">
-              <li><div class="t-c-red"></div></li>
-              <li><div class="t-c-blue"></div></li>
-              <li><div class="t-c-green"></div></li>
-            </ul>
-          </transition>
-        <!--</li> -->
-        <!-- <li>
-            <h3>貼圖</h3>
+       <!-- 字的大小 -->
+        <div v-on:click="show = !show">
+          <h5>字級</h5>
+          <h1>A</h1>
+        </div>
+        <transition name="ul">
+          <ul v-if="show" class="ul-box">
+            <li><h2>A</h2></li>
+            <li><h4>A</h4></li>
+            <li><h6>A</h6></li>
+          </ul>
+        </transition>
+       <!-- 字的顏色 -->
+        <div v-on:click="active = ! active">
+          <h5>字的顏色</h5>
+          <div class="tool-color"></div>
+        </div>
+        <transition name="ul">
+          <ul v-if="active" class="ul-box">
+            <li><div class="t-c-red"></div></li>
+            <li><div class="t-c-blue"></div></li>
+            <li><div class="t-c-green"></div></li>
+          </ul>
+        </transition>
+      <!-- 貼圖 -->
+          <div v-on:click="picture = ! picture">
+            <h5>貼圖</h5>
             <div><img src="../../public/images/pic/crown.png" alt="" /></div>
-            <ul>
+          </div>
+          <transition name="ul">
+            <ul v-if="picture" class="ul-box">
               <li>
                 <img src="../../public/images/pic/crown.png" id="texture1" alt="" />
               </li>
@@ -49,73 +48,149 @@
                 <img src="../../public/images/pic/confetti.png" id="texture4" alt="" />
               </li>
             </ul>
-          </li>
-          <li>
-            <h3>DIY綠拿鐵</h3>
+          </transition>
+
+          <!-- DIY綠拿鐵 -->
+          <div v-on:click="diyGreen = ! diyGreen">
+            <h5>DIY綠拿鐵</h5>
             <div><img src="../../public/images/pic/dragon-fruit.png" alt="" /></div>
-            <ul>
+          </div>
+          <transition name="ul">
+            <ul v-if="diyGreen" class="ul-box">
               <li><img src="../../public/images/pic/dragon-fruit.png" alt="" /></li>
               <li></li>
             </ul>
-          </li>
-          <li>
-            <h3>健康餐</h3>
+          </transition>
+          <!-- 健康餐 -->
+          <div v-on:click="healthMeal = ! healthMeal">
+            <h4>健康餐</h4>
             <div><img src="../../public/images/pic/fried-rice.png" alt="" /></div>
-            <ul>
+          </div>
+          <transition name="ul">
+            <ul v-if="healthMeal" class="ul-box">
               <li><img src="../../public/images/pic/fried-rice.png" alt="" /></li>
             </ul>
-          </li>
-          <li>
-            <h3>推薦<br />綠拿鐵</h3>
-            <ul>
+          </transition>
+
+          <!-- 推薦綠拿鐵 -->
+          <div v-on:click="sugGreen = ! sugGreen">
+            <h5>推薦<br />綠拿鐵</h5>
+          </div>
+          <transition name="ul">
+            <ul v-if="sugGreen" class="ul-box">
               <li>
-                <h4>火紅綠拿鐵</h4>
+                <h6>火紅綠拿鐵</h6>
                 <img src="../../public/images/pic/drink.png" alt="" />
               </li>
               <li>
-                <h4>經典綠拿鐵</h4>
+                <h6>經典綠拿鐵</h6>
                 <img src="../../public/images/pic/orange-juice.png" alt="" />
               </li>
               <li>
-                <h4>清爽綠拿鐵</h4>
+                <h6>清爽綠拿鐵</h6>
                 <img src="../../public/images/pic/juice.png" alt="" />
               </li>
             </ul>
-          </li>
+          </transition>
 
           <div class="tool-try">
-            <h4>漸進式</h4>
-            <h4>體驗式</h4>
-            <h4>全面式</h4>
-          </div> -->
-        <!-- </ul> -->
-      </div>
-
-      <!-- 月曆大小 -->
-      <div id="calendarsite">
-        <!-- 顯示年/月   -->
-        <div id="topTitle">
-          <div class="rightOption">
-            <div id='teamTimeBtn' class='btnStyle'>下午茶</div>
-            <div id='nightBtn' class='btnStyle'>宵夜</div>
+            <h5>漸進式</h5>
+            <h5>體驗式</h5>
+            <h5>全面式</h5>
           </div>
-        </div>
-        <!-- 月曆 -->
-        <div id="calendarTag"></div>
-        <!-- 下方按鈕 -->
-        <div id="bottomBtn"></div>
       </div>
+      <!-- 月曆大小 -->
+<div id="calendarsite">
+  <!-- 顯示年/月   -->
+  <div id="topTitle">
+    <h3 class='dateWord'>2021年6月</h3>
+      <div class="rightOption">
+        <div id='teamTimeBtn' class='btnStyle'>下午茶</div>
+        <div id='nightBtn' class='btnStyle'>宵夜</div>
       </div>
-    <!-- </div>
   </div>
- </div> -->
+  <!-- 月曆 -->
+  <div id="calendarTag">
+   <table>
+    <thead>
+      <tr class="th-row">
+        <th></th>
+        <th>一</th>
+        <th>二</th>
+        <th>三</th>
+        <th>四</th>
+        <th>五</th>
+        <th>六</th>
+        <th>日</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>早</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>中</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>晚</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>備註</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+  </div>
+      <!-- 下方按鈕 -->
+   <div id="bottomBtn">
+    <div id='saveBtn' class='btnStyle'>儲存</div>
+    <div id='previewBtn' class='btnStyle'>預覽</div>
+    <div id='downloadBtn' class='btnStyle'>下載PDF</div>
+   </div>
+   </div>
+  </div>
+
+ </div>
+ </div>
 </template>
 <script>
+
 export default {
   data () {
     return {
       show: false,
-      active: false
+      active: false,
+      picture: false,
+      diyGreen: false,
+      healthMeal: false,
+      sugGreen: false
     }
   }
 }
