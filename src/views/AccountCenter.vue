73b7acdfcfe346b701ed8d3210256c90cf9c3f05
Kate <katelovejesus1110@gmail.com>
2021-06-04 12:28:51 +0800

    調整會員中心樣式

--- src/views/AccountCenter.vue
+++ src/views/AccountCenter.vue
@@ -31,71 +31,77 @@
       </div>
     </b-tab>
     <b-tab title="會員中心" class="account-center">
-      <div class="account-photo">
-        <div><font-awesome-icon icon="user-circle"/></div>
-        <h5>大頭貼照片</h5>
-        <input type="button" value="選擇檔案" />
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-6 col-sm-3 col-md-3 col-lg-2">
+            <div class="account-photo">
+             <div><font-awesome-icon icon="user-circle"/></div>
+             <h5>大頭貼照片</h5>
+             <input type="button" value="選擇檔案" />
+            </div>
+          </div>
+          <div class="col-xs-6 col-sm-9 col-md-9 col-lg-10">
+            <div class="account-detail">
+              <div class="word-row">
+                <h5>您的名字/稱謂</h5>
+                <p>(必填)</p>
+                <input type="text" value="請輸入您的名字" />
+              </div>
+              <div class="word-row">
+                <h5>性別</h5>
+                <p>(必填)</p>
+                <input type="checkbox" name="" id="" />男性
+                <input type="checkbox" name="" id="" />女性
+              </div>
+              <div class="word-row">
+                <h5>出生年月日</h5>
+                <p>(必填)</p>
+                <h6>民國</h6>
+                <select name="" id=""></select>
+                <h6>年</h6>
+                <select name="" id=""></select>
+                <h6>月</h6>
+                <select name="" id=""></select>
+                <h6>日</h6>
+              </div>
+              <div class="word-row">
+                <h5>地址</h5>
+                <p>(必填)</p>
+                <h6>郵遞區號</h6>
+                <input type="text" name="" id="" />
+                <h6>縣市</h6>
+                <select name="" id=""></select>
+                <h6>地址</h6>
+                <input type="text" name="" id="" />
+              </div>
+              <div class="word-row">
+                <h5>電話</h5>
+                <p>(必填)</p>
+                <input type="text" name="" id="" />
+                <h6>-</h6>
+                <input type="text" name="" id="" />
+              </div>
+              <div class="word-row">
+                <h5>手機</h5>
+                <p>(必填)</p>
+                <input type="text" name="" id="" />
+              </div>
+              <hr/>
+              <div class="ourself-check">
+                <input type="radio" name="" id="" />自我檢視健康
+              </div>
+              <div class="ourself-check">
+                <input type="radio" name="" id="" />綠拿鐵ＤＩＹ
+              </div>
+              <div class="ourself-check">
+                <input type="radio" name="" id="" />綠拿鐵三餐規劃
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div class="account-detail">
-        <div class="word-row">
-          <h5>您的名字/稱謂</h5>
-          <p>(必填)</p>
-          <input type="text" value="請輸入您的名字" />
-        </div>
-        <div class="word-row">
-          <h5>性別</h5>
-          <p>(必填)</p>
-          <input type="checkbox" name="" id="" />男性
-          <input type="checkbox" name="" id="" />女性
-        </div>
-        <div class="word-row">
-          <h5>出生年月日</h5>
-          <p>(必填)</p>
-          <h6>民國</h6>
-          <select name="" id=""></select>
-          <h6>年</h6>
-          <select name="" id=""></select>
-          <h6>月</h6>
-          <select name="" id=""></select>
-          <h6>日</h6>
-        </div>
-        <div class="word-row">
-          <h5>地址</h5>
-          <p>(必填)</p>
-          <h6>郵遞區號</h6>
-          <input type="text" name="" id="" />
-          <h6>縣市</h6>
-          <select name="" id=""></select>
-          <h6>地址</h6>
-          <input type="text" name="" id="" />
-        </div>
-        <div class="word-row">
-          <h5>電話</h5>
-          <p>(必填)</p>
-          <input type="text" name="" id="" />
-          <h6>-</h6>
-          <input type="text" name="" id="" />
-        </div>
-        <div class="word-row">
-          <h5>手機</h5>
-          <p>(必填)</p>
-           <input type="text" name="" id="" />
-        </div>
-        <hr />
-        <div class="ourself-check">
-          <input type="radio" name="" id="" />自我檢視健康
-        </div>
-        <div class="ourself-check">
-           <input type="radio" name="" id="" />綠拿鐵ＤＩＹ
-        </div>
-        <div class="ourself-check">
-           <input type="radio" name="" id="" />綠拿鐵三餐規劃
-        </div>
-      </div>
-
     </b-tab>
   </b-tabs>
-
 </div>
 </template>
 
