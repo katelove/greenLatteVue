a93699286b52c5673706548750a5858cbf3aff2d
Kate <katelovejesus1110@gmail.com>
2021-06-23 14:06:44 +0800

    輪播圖內文樣式

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -4,11 +4,9 @@
       id="carousel-1"
       v-model="slide"
       :interval="3000"
-      class="carousel-style carousel-img"
+      class="carousel-style"
       controls
       indicators
-      background="#ababab"
-      style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
@@ -17,9 +15,10 @@
       <b-carousel-slide
         v-for="item in carouselItems"
         :key="item.id"
-       :img-src=item.image
+        :img-src=item.image
         :caption="item.caption"
         :text="item.text"
+        class="wordTitle"
       ></b-carousel-slide>
     </b-carousel>
   </div>
@@ -39,13 +38,13 @@ export default {
           id: 2,
           caption: '崇尚自然飲食',
           image: require('../../public/images/carousel/2.png'),
-          text: '品嚐食物原汁原味'
+          text: '▶品嚐食物原汁原味'
         },
         {
           id: 3,
           caption: '創造您的口感',
           image: require('../../public/images/carousel/3.png'),
-          text: 'DIY天然蔬果汁'
+          text: '▶DIY天然蔬果汁'
         }
       ],
       slide: 0,
@@ -67,10 +66,23 @@ export default {
 .carousel-style{
   width: 100%;
   min-height: 100%;
-}
-.carousel-img{
   width:"1024px";
   height:"480px";
   color: white;
+}
+.wordTitle{
+  text-shadow: 2px 2px 10px #2f5a28;
+  h3{
+  font-size: 50px;
+  background: rgb(250, 249, 249,0.5);
+  font-weight: bold;
+  font-family: '微軟正黑體';
+  text-align: left;
+  padding: 10px;
+   }
+  p{
+    font-size: 25px;
+     text-align: left;
+  }
 }
 </style>
