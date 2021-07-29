da367647ebb008e5d3d66a9cf6514b1a0eb246a0
Kate <katelovejesus1110@gmail.com>
2021-07-29 10:56:49 +0800

    adjust greenLatte RWD

--- src/components/GreenLatte.vue
+++ src/components/GreenLatte.vue
@@ -1,49 +1,53 @@
 <template>
   <div class="container">
-          <div class="row align-items-center">
-            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-              <div class="greenTitle">
-                <h3>什麼是</h3>
-                <h1>綠拿鐵</h1>
-              </div>
-            </div>
-            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-              <div class="greenPic">
-                <div class="col-sm-12 col-md-12 col-lg-3 col-xl-3">
-                  <div class="vg_pic">
-                    <img src="../../public/images/company/vg.png" alt="" />
-                    <h3>蔬菜 x 2</h3>
-                  </div>
-                </div>
-                <div class="col-sm-12 col-md-12 col-lg-1 col-xl-1">
-                  <img src="../../public/images/company/add.png" alt="" />
-                </div>
-                <div class="col-sm-12 col-md-12 col-lg-3 col-xl-3">
-                  <div class="fruit_pic">
-                    <img src="../../public/images/company/apple.png" alt="" />
-                    <h3>水果 x 1</h3>
-                  </div>
-                </div>
-                <div class="col-sm-12 col-md-12 col-lg-1 col-xl-1">
-                  <img src="../../public/images/company/equal.png" alt="" />
-                </div>
-                <div class="col-sm-12 col-md-12 col-lg-3 col-xl-3">
-                  <img src="../../public/images/company/greenLatte.png" alt="" />
-                </div>
-              </div>
+    <div class="row align-items-center">
+      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="greenTitle">
+          <h3>什麼是</h3>
+          <h1>綠拿鐵</h1>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+        <div class="greenPic">
+          <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+            <div class="vg_pic">
+              <img src="../../public/images/company/vg.png" alt="" />
+              <h3>蔬菜 x 2</h3>
             </div>
           </div>
-          <div class="greenWord">
-            <h3>利用天然食物，天然纖維讓身體健康取得平衡</h3>
-            <h3>*注意水果攝取不要太多，以免果糖攝取過量。</h3>
+          <div class="col-sm-12 col-md-1 col-lg-1 col-xl-1">
+            <img src="../../public/images/company/add.png" alt="" />
           </div>
-          <div class="col-sm-12 col-md-12">
-            <div class="workerBtn diyBtn">
-              <a href="#sec5" @click="goDiy()">DIY</a>
-              <a href="#sec7" @click="threeMeat()">三餐規劃</a>
+          <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+            <div class="fruit_pic">
+              <img src="../../public/images/company/apple.png" alt="" />
+              <h3>水果 x 1</h3>
             </div>
+          </div>
+          <div class="col-sm-12 col-md-1 col-lg-1 col-xl-1">
+            <img src="../../public/images/company/equal.png" alt="" />
+          </div>
+          <div class="col-sm-12 col-md-3 col-lg-3 col-xl-3">
+            <img src="../../public/images/company/greenLatte.png" alt="" />
           </div>
         </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-12 greenWord">
+        <h3>利用天然食物，天然纖維讓身體健康取得平衡</h3>
+        <h3>*注意水果攝取不要太多，以免果糖攝取過量。</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-12">
+        <div class="workerBtn diyBtn">
+          <a href="#sec5" @click="goDiy()">DIY</a>
+          <a href="#sec7" @click="threeMeat()">三餐規劃</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -60,4 +64,6 @@ export default {
 }
 </script>
 
-<style lang="scss">@import "../scss/greenLatte.scss"; </style>
+<style lang="scss">
+@import '../scss/greenLatte.scss';
+</style>
